713dade6a4f163524aac7a928428afa6ccfcadf4
Jonathan Doucette <jon@digitalforgedesign.com>
2023-07-23 22:13:55 -0400

    all the golang langing being langed

--- bill.go
+++ bill.go
@@ -9,25 +9,24 @@ type bill struct {
 	name string
 	items map[string]float64
 	tip float64
-	
 }
 
 // make new bill
 func newBill(name string) bill {
 	b := bill{
 		name: name,
-		items: map[string]float64{"pie": 5.99, "cake": 3.99},
+		items: map[string]float64{},
 		tip: 0,
 	}
 
 	return b
 }
 
-//receiver functions
+//receiver functions - these also only create a copy 
 // format bill
 
 // this limits this function to only bill objects
-func (b bill) format() string {
+func (b *bill) format() string {
 	// this is just a copy of the bill not the real one
 	fs := "Bill breakdown: \n"
 	var total float64 = 0
@@ -39,8 +38,30 @@ func (b bill) format() string {
 		total += v
 	}
 
+	//add tip
+	fs += fmt.Sprintf("%-25v ...$%v\n", "tip", b.tip)
+	// total before tip
+	fs += fmt.Sprintf("%-25v ...$%0.2f\n", "total before tip:", total)
 	// total 
-	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total)
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total+b.tip)
+
 
 	return fs
+}
+
+// update tip
+func (b *bill) updateTip(tip float64) {
+	// the '*' is the de-referencer but in a struct we need to put it in parenthasis
+	// if you're reading this in the future it instead references the pointer instead of the copy
+	// so the real value gets updated
+
+	// One last caveat - we really don't need the parenthesis when de-referecning a struck
+	// go will automaitically do it
+	//(*b).tip = tip
+	b.tip = tip
+}
+
+// add item to the bill
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
 }
--- main.go
+++ main.go
@@ -16,11 +16,97 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) (string, error){
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+
+}
+
+func createBill() bill{
+	// bufio is a package that makes it so you can interact with the terminal
+	// NewReader is a method and we call 'os.Stdin' which means
+	// the operating systems standard input
+	reader := bufio.NewReader(os.Stdin)
+
+	// the underscore is a placeholder for an error 
+	// when we use the reader with getInput function we created
+	name, _ := getInput("Create a new bill name:", reader)
+
+	// we then get rid of the whitespace by adding the strings package then 
+	// the method TrimSpace and pass in the name
+	name = strings.TrimSpace(name)
+
+	b := newBill(name)
+	fmt.Println("Created the bill - ", b.name)
+
+	return b
+}
+
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+	option, _ := getInput("Choose Option (a - add item, s - save bill, t - add tip):", reader)
+	
+	switch option {
+	case "a":
+			name, _ := getInput("Item name:", reader)
+			price, _ := getInput("Item price:", reader)
+
+			// new package strconv - string conversion to convert the string the user input to a float64
+			p, err := strconv.ParseFloat(price, 64)
+			if err != nil {
+				fmt.Println("The price must be a number")
+				promptOptions(b)
+			}
+			b.addItem(name, p)
+			fmt.Println("Item added:", name, price)
+			// fire off the function again so the user can add a new item8
+			promptOptions(b)
+	case "t":
+			tip, _ := getInput("Enter tip amount ($):", reader)
+			
+			t, err := strconv.ParseFloat(tip, 64)
+			if err != nil {
+				fmt.Println("The tip must be a number")
+				promptOptions(b)
+			}
+			b.updateTip(t)
+			fmt.Println("Tip added:", tip)
+			// fire off the function again so the user can add a new item8
+			promptOptions(b)
+	case "s":
+			fmt.Println("you chose to save the bill", b)
+	default:
+		fmt.Println("that was not a valid option..")
+		// if they don't choose a letter from the options then 
+		// this fires off the function again
+		promptOptions(b)
+	}
+
+}
 
 func main(){
-	mybill := newBill("Mario's bill")
+	mybill := createBill()
+	promptOptions(mybill)
+	//fmt.Println(mybill)
+	// mybill := newBill("Mario's bill")
 
-	fmt.Println(mybill.format())
+	// mybill.updateTip(10)
+	// mybill.addItem("onion soup", 4.50)
+	// mybill.addItem("veg pie", 8.95)
+	// mybill.addItem("cake", 8.95)
+	// mybill.addItem("cheese burger", 8.95)
+	
+
+	// fmt.Println(mybill.format())
 
 }
